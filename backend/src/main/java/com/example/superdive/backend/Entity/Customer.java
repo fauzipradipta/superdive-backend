5e34e0fa27cc0e1706d3eba38dc50e148c9c2138
fauzipradipta <dipta0988@gmail.com>
2025-03-07 14:02:07 +0700

    add Entity/Customer.java

--- backend/src/main/java/com/example/superdive/backend/Entity/Customer.java
+++ backend/src/main/java/com/example/superdive/backend/Entity/Customer.java
@@ -1,5 +1,24 @@
 package com.example.superdive.backend.Entity;
 
-public class Customer {
+import java.sql.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table( name = "customers")
+@Data
+public class Customer {
+	
+	private Long id;
+	private String name; 
+	private String email;
+	private Date dob;
+	private String phoneNum; 
+	boolean isDiver;
+	
+//	@OneToMany(mappedBy = "customer", cascade= CascadeType.All,orphanRemoval = true )
+	
 }

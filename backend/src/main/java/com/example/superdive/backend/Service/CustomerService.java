e1597b06f76f70f10ac8d440ce40a266f47ea3ca
Syawadhilah Pradipta <dipta0988@gmail.com>
2025-03-09 23:50:02 +0700

    add service and controller

---
+++ backend/src/main/java/com/example/superdive/backend/Service/CustomerService.java
@@ -0,0 +1,33 @@
+package com.example.superdive.backend.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.superdive.backend.Entity.Customer;
+import com.example.superdive.backend.Repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+	
+	@Autowired
+	private CustomerRepository customerRepository; 
+	
+	public Customer createCustomer(Customer customer) {
+		return customerRepository.save(customer);
+	}
+	
+	public Customer getCustomerById(Long id) {
+		return customerRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Customer not found"));
+	}
+	
+	public List<Customer> getAllCustomer(){
+		return customerRepository.findAll();
+	}
+	
+	public void deleteCustomer(Long id) {
+		customerRepository.deleteById(id);
+	}
+}
---
+++ backend/src/main/java/com/example/superdive/backend/controller/CustomerController.java
@@ -0,0 +1,25 @@
+package com.example.superdive.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.superdive.backend.Entity.Customer;
+import com.example.superdive.backend.Service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(customer));
+    }
+}

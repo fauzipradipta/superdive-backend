5309ea59f6f928585086b90819d6675144c09915
Syawadhilah Pradipta <dipta0988@gmail.com>
2025-04-11 12:29:51 +0700

    edit divingdatadto

--- backend/src/main/java/com/example/superdive/backend/Entity/Customer.java
+++ backend/src/main/java/com/example/superdive/backend/Entity/Customer.java
@@ -18,9 +18,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
--- backend/src/main/java/com/example/superdive/backend/dto/DivingDataDTO.java
+++ backend/src/main/java/com/example/superdive/backend/dto/DivingDataDTO.java
@@ -1,8 +1,5 @@
 package com.example.superdive.backend.dto;
 
-import java.time.LocalDate;
-import java.util.List;
-
 public class DivingDataDTO {
 	
 	

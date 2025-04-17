b13ebeef6302e6ebcec6240fd4d5338de9412836
fauzipradipta <dipta0988@gmail.com>
2025-04-17 15:45:06 +0700

    base product

--- backend/src/main/java/com/example/superdive/backend/Entity/Product.java
+++ backend/src/main/java/com/example/superdive/backend/Entity/Product.java
@@ -11,9 +11,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "product")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +22,56 @@ public class Product {
     private String rent;
     private String service;
     private String retail;
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getTrip() {
+		return trip;
+	}
+	public void setTrip(String trip) {
+		this.trip = trip;
+	}
+	public String getRent() {
+		return rent;
+	}
+	public void setRent(String rent) {
+		this.rent = rent;
+	}
+	public String getService() {
+		return service;
+	}
+	public void setService(String service) {
+		this.service = service;
+	}
+	public String getRetail() {
+		return retail;
+	}
+	public void setRetail(String retail) {
+		this.retail = retail;
+	}
+	public Product(Long id, String course, String trip, String rent, String service, String retail) {
+		super();
+		this.id = id;
+		this.course = course;
+		this.trip = trip;
+		this.rent = rent;
+		this.service = service;
+		this.retail = retail;
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+    
+    
 }

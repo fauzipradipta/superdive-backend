d393cb490a0dc011fbfafa7c0a1fbd5abfc8f47a
fauzipradipta <dipta0988@gmail.com>
2025-04-25 16:27:18 +0700

    change Product entity

--- backend/src/main/java/com/example/superdive/backend/entity/Product.java
+++ backend/src/main/java/com/example/superdive/backend/entity/Product.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +21,7 @@ public class Product {
 	
 	private ProductType type;
 	private String details;
+	private int qty;
 	private BigDecimal price;
 	
 	public Long getId() {
@@ -41,22 +43,40 @@ public class Product {
 	public void setDetails(String details) {
 		this.details = details;
 	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
 	public BigDecimal getPrice() {
 		return price;
 	}
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
-	public Product(Long id, ProductType type, String details, BigDecimal price) {
+	public Product(Long id, ProductType type, String details, int qty, BigDecimal price, Customer customer) {
 		super();
 		this.id = id;
 		this.type = type;
 		this.details = details;
+		this.qty = qty;
 		this.price = price;
+		this.customer = customer;
 	}
 	public Product() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+	
+	@ManyToOne
+	private Customer customer;
+	
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 	
 	

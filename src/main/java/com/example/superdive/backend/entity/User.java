24982a5d7e0f035dd74da264851f8eb8efa04c9b
Syawadhilah Pradipta <fpradipta@yahoo.com>
2026-07-17 14:33:11 +0700

    working on users

---
+++ src/main/java/com/example/superdive/backend/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.superdive.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+
+}

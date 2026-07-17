8130e55071cb2a31435f9205d4e04ca3bdbed7aa
Syawadhilah Pradipta <fpradipta@yahoo.com>
2026-07-17 16:22:14 +0700

    working on UsersDTO

---
+++ src/main/java/com/example/superdive/backend/dto/Request/UserRequestDTO.java
@@ -0,0 +1,9 @@
+package com.example.superdive.backend.dto.Request;
+
+public class UserRequestDTO{
+  public Long id;
+  public String firstName;
+  public String lastname;
+  public String email;
+  public String password;
+}
